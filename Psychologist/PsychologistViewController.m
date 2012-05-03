11373016ef0db3e0c22544823b915d35d786398a
Thadeu <thadeu.russo@caelum.com.br>
2012-05-02 21:56:20 -0300

    Initial Commit

---
+++ Psychologist/PsychologistViewController.m
@@ -0,0 +1,34 @@
+//
+//  PsychologistViewController.m
+//  Psychologist
+//
+//  Created by Thadeu Carmo on 5/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PsychologistViewController.h"
+
+@interface PsychologistViewController ()
+
+@end
+
+@implementation PsychologistViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
